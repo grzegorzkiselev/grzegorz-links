3be8db7d57a8eba8cfa0f41e6d3135b59009a6fc
grzegorzkiselev <egorkakiselev@gmail.com>
2023-04-18 13:07:55 +0300

    Новый (нормальный) алгоритм выхода из прелодера, обновленные иконки-ссылки

--- src/preloader/preloader.js
+++ src/preloader/preloader.js
@@ -1,134 +1,112 @@
-import { Scene, PerspectiveCamera, Vector2, ShaderMaterial, DoubleSide, Mesh, PlaneBufferGeometry, WebGLRenderer } from "../../static/utilities/three.min.js";
-import fragmentShader from "./fragmentShader.glsl"
-import vertexShader from "./vertexShader.glsl"
 import {
-  gsap
-} from 'gsap';
+  Scene,
+  PerspectiveCamera,
+  Vector2,
+  ShaderMaterial,
+  DoubleSide,
+  Mesh,
+  PlaneBufferGeometry,
+  WebGLRenderer,
+} from "../../static/utilities/three.min.js";
+import fragmentShader from "./fragmentShader.glsl";
+import vertexShader from "./vertexShader.glsl";
+import { gsap } from "gsap";
 
-// Canvas
-const canvas = document.querySelector('.preloader')
+const canvas = document.querySelector(".preloader");
 
-// Scene
-const scene = new Scene()
+const scene = new Scene();
 
-// Loaders
-let stopModule = false
+let stopModule = false;
 document.addEventListener("moduleLoaded", (e) => {
-  gsap.delayedCall(1.5, () => {
+  gsap.delayedCall(3, () => {
     gsap.to(overlayMaterial.uniforms.uRadius, {
       duration: 2,
-      value: 4.00
+      value: 4.0,
     });
     gsap.to(overlayMaterial.uniforms.uBlur, {
-      duration: 3,
-      value: 0.0
+      duration: 1.5,
+      value: 0.0,
     });
     setTimeout(() => {
-        canvas.remove()
-        stopModule = true
-    }, "2000")
-  })
-})
+      canvas.remove();
+      stopModule = true;
+    }, "2000");
+  });
+});
 
-// Sizes
 const frame = document.querySelector(".preloader");
 
 const sizes = {
   width: window.innerWidth * 2,
-  height: window.innerHeight * 2
-}
+  height: window.innerHeight * 2,
+};
 
 const mouse = new Vector2(sizes.width / 2, sizes.height / 2);
-window.addEventListener('mousemove', (event) => {
+window.addEventListener("mousemove", (event) => {
   mouse.x = event.clientX / window.innerWidth - 0.5;
   mouse.y = -(event.clientY / window.innerHeight - 0.5);
-})
+});
 
-// Preloader
 const overlayMaterial = new ShaderMaterial({
   precision: "lowp",
-  // transparent: true,
   side: DoubleSide,
   uniforms: {
     uAlpha: {
-      value: 0.5
+      value: 0.5,
     },
     uMouse: {
-      value: mouse
+      value: mouse,
     },
     uTime: {
-      value: 0
+      value: 0,
     },
     uRes: {
-      value: new Vector2(window.innerWidth, window.innerHeight)
+      value: new Vector2(window.innerWidth, window.innerHeight),
     },
     uRadius: {
-      value: 0.01
+      value: 0.01,
+      // value: Math.sqrt((sizes.width / 2) ** 2 + (sizes.height / 2) ** 2),
     },
     uBlur: {
-      value: 4.0
-    }
+      value: 4.0,
+    },
   },
   vertexShader: vertexShader,
   fragmentShader: fragmentShader,
   defines: {
-    PR: window.devicePixelRatio.toFixed(2)
-  }
+    PR: window.devicePixelRatio.toFixed(2),
+  },
+});
 
-})
+const overlay = new Mesh(new PlaneBufferGeometry(2, 2, 1, 1), overlayMaterial);
+canvas.style.backgroundColor = "unset";
+scene.add(overlay);
 
-const overlay = new Mesh(
-  new PlaneBufferGeometry(2, 2, 1, 1),
-  overlayMaterial
-)
-canvas.style.backgroundColor = "unset"
-scene.add(overlay)
+window.addEventListener("resize", () => {
+  sizes.width = frame.clientWidth;
+  sizes.height = frame.clientHeight;
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(2);
+});
 
-window.addEventListener('resize', () => {
-  // Update sizes
-  sizes.width = frame.clientWidth
-  sizes.height = frame.clientHeight
+const camera = new PerspectiveCamera(0, sizes.width / sizes.height, 0, 0);
+scene.add(camera);
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(2)
-})
-
-// Base camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-const camera = new PerspectiveCamera(0, sizes.width / sizes.height, 0, 0)
-// camera.position.set(4, 1, -4)
-scene.add(camera)
-
-
-// Renderer
 const renderer = new WebGLRenderer({
   canvas: canvas,
   antialias: false,
   alpha: true,
-  powerPreference: "high-performance"
-})
+  powerPreference: "high-performance",
+});
 
-renderer.setSize(sizes.width, sizes.height)
-
-// Animation
-// const clock = new THREE.Clock()
+renderer.setSize(sizes.width, sizes.height);
 
 const tick = () => {
   if (!stopModule) {
-
-    overlay.material.uniforms.uTime.value += 0.01
-
-    // controls.update()
-
-    renderer.render(scene, camera)
-
+    overlay.material.uniforms.uTime.value += 0.01;
+    renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
   }
-}
+};
 
-tick()
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+tick();
